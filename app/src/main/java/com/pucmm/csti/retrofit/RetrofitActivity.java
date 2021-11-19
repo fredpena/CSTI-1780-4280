d937dbd3943ead3071457410472d52684f3e89a4
Freddy Peña <f.ant.pena@gmail.com>
2021-11-19 10:24:46 -0400

    Refactoring...

--- app/src/main/java/com/pucmm/csti/retrofit/RetrofitActivity.java
+++ app/src/main/java/com/pucmm/csti/retrofit/RetrofitActivity.java
@@ -1,7 +1,10 @@
 package com.pucmm.csti.retrofit;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.gson.*;
 import com.pucmm.csti.R;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -9,77 +12,68 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.util.List;
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class RetrofitActivity extends AppCompatActivity {
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://curly-chipmunk-76.loca.lt")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        button = findViewById(R.id.button);
 
+        button.setOnClickListener(v -> {
 
-        Call<List<Userr>> userCall = retrofit.create(UserApiService.class).getAll();
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(Date.class, dateJsonDeserializer);
 
-        System.out.println("enqueue");
-        userCall.enqueue(new Callback<List<Userr>>() {
-            @Override
-            public void onResponse(Call<List<Userr>> call, Response<List<Userr>> response) {
-                response.body().forEach(obj -> {
-                    System.out.println(obj.getFirstName());
-                });
-            }
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("http://137.184.110.89:7002")
+                    .addConverterFactory(GsonConverterFactory.create(gsonBuilder.create()))
+                    .build();
 
-            @Override
-            public void onFailure(Call<List<Userr>> call, Throwable t) {
-                System.err.println(t.getLocalizedMessage());
-            }
+            Call<List<Userr>> userCall = retrofit.create(UserApiService.class).getAll();
+
+            System.out.println("enqueue");
+            userCall.enqueue(new Callback<List<Userr>>() {
+                @Override
+                public void onResponse(Call<List<Userr>> call, Response<List<Userr>> response) {
+                    System.out.println("onResponse...");
+                    response.body().forEach(obj -> {
+                        System.out.println(obj.toString());
+                    });
+                }
+
+                @Override
+                public void onFailure(Call<List<Userr>> call, Throwable t) {
+                    System.err.println("onFailure...");
+                    t.printStackTrace();
+                }
+            });
+
         });
 
-
-//        Post post = new Post();
-//        post.setUserId(1);
-//        post.setTitle("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
-//        post.setText("quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto");
-//
-//        Call<Post> postCall = retrofit.create(APIService.class).create(post);
-//
-//        postCall.enqueue(new Callback<Post>() {
-//            @Override
-//            public void onResponse(Call<Post> call, Response<Post> response) {
-//                System.err.println("code: " + response.code());
-//                System.err.println(response.body().getText());
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Post> call, Throwable t) {
-//                System.err.println(t.getLocalizedMessage());
-//            }
-//        });
-
-//        Call<List<Comment>> listCall = retrofit.create(APIService.class).getCommentsByPost(2);
-//        listCall.enqueue(new Callback<List<Comment>>() {
-//            @Override
-//            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
-//
-//                response.body().forEach(obj -> {
-//                    System.out.println(obj.getName());
-//                });
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Comment>> call, Throwable t) {
-//                System.err.println(t.getLocalizedMessage());
-//
-//            }
-//        });
-
     }
+
+    private final JsonDeserializer<Date> dateJsonDeserializer = (json, typeOfT, context) -> {
+        try {
+            final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+            dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+            final String strDate = dateFormat.format(new Date(json.getAsLong()));
+            System.out.println(strDate);
+            return new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+        } catch (ParseException e) {
+            return null;
+        }
+    };
+
 }
